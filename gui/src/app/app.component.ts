cff5960d8ff249b734c41b22b0d9fa8afb7a6d31
tame0001 <thirawat.tam@gmail.com>
2023-03-23 16:54:34 -0400

    demo website to show temp and humidity

---
+++ gui/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnDestroy{
+  title = 'Boiler Robotics';
+  private humidityTopic!: Subscription;
+  public humidity!: string;
+  private temperatureTopic!: Subscription;
+  public temperature!: string;
+
+  constructor(private _mqttService: MqttService) { 
+    this.humidityTopic = this._mqttService.observe(
+      'purdue-dac/telemetry/huminity'
+    ).subscribe((message: IMqttMessage) => {
+      this.humidity = message.payload.toString();
+      console.log(this.humidity);
+    });
+    this.temperatureTopic = this._mqttService.observe(
+      'purdue-dac/telemetry/temperature'
+    ).subscribe((message: IMqttMessage) => {
+      this.temperature = message.payload.toString()
+      console.log(this.temperature)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.humidityTopic.unsubscribe();
+    this.temperatureTopic.unsubscribe();
+  }
+
+}
