4633e8c2566e63725238c43b025f01a602dec31c
tame0001 <thirawat.tam@gmail.com>
2023-05-26 01:01:45 -0400

    remove mqtt

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { RouterOutlet } from '@angular/router';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { EChartsOption } from 'echarts';
 import { GaugeComponent } from './gauge/gauge.component';
-import * as mqtt from 'mqtt-browser';
 
 @Component({
   selector: 'app-root',
@@ -29,7 +28,6 @@ export class AppComponent implements OnDestroy {
   private temperatureTopic!: Subscription;
   public temperature!: string;
   public timestamp = new Date();
-  private client: mqtt.Client = mqtt.connect('ws://66.253.158.154:9001/mqtt');
 
   public temperatureGaugeOption: EChartsOption = {
     title: {
@@ -74,21 +72,6 @@ export class AppComponent implements OnDestroy {
         this.temperature = message.payload.toString();
         this.isData = true;
       });
-
-    this.client.on('connect', () => {
-      console.log('Connected to the broker!');
-      this.client.subscribe([
-        'purdue-dac/telemetry/temperature',
-        'purdue-dac/telemetry/humidity'
-      ], () => {
-        console.log('Sub completed');
-      })
-    });
-
-    this.client.on('message', (topic, payload) => {
-      console.log(topic);
-      console.log(payload.toString());
-    })
     
   }
 
