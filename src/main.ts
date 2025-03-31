44cbd0a6c798dc3313627b8c0f9569ed2eca6946
tame0001 <thirawat.tam@gmail.com>
2025-03-30 23:37:23 -0400

    upgrade angular core to 18

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule),
-  ],
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
 }).catch((err) => console.error(err));
