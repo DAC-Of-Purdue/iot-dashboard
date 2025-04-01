3e4122b51676bee0e29b09b780a15d7482193737
tame0001 <thirawat.tam@gmail.com>
2025-04-01 11:42:25 -0400

    add soil moisture

--- src/app/ag-week/ag-week.component.ts
+++ src/app/ag-week/ag-week.component.ts
@@ -16,7 +16,7 @@ interface Sensor {
   name: string;
   topic: string;
   type: string;
-  data?: DhtDataInterface;
+  data?: DhtDataInterface | number;
 }
 
 @Component({
@@ -42,14 +42,19 @@ export class AgWeekComponent {
 
   sensors: Sensor[] = [
     {
-      name: 'Sensor 1',
+      name: 'Pot 1',
       topic: 'ag-week-demo-1',
       type: 'dht',
     },
     {
-      name: 'Sensor 2',
+      name: 'Pot 2',
       topic: 'ag-week-demo-2',
       type: 'dht',
+    },
+    {
+      name: 'Pot 3',
+      topic: 'ag-week-demo-3',
+      type: 'soil',
     },
   ];
 
@@ -63,37 +68,44 @@ export class AgWeekComponent {
     client.on('connect', () => {
       console.log('Connected to broker.');
       client.subscribe(`purdue-dac/sensor/#`);
+      // client.publish('purdue-dac/command/ag-week-demo-1', '1', { qos: 2 });
     });
     // listen for messages
     client.on('message', (topic, message, _) => {
-      // extract device name and data
-      let regexpTopic = new RegExp('purdue-dac/sensor/(.*)');
-      let regexpPayload = new RegExp('(.*):(.*):(.*)');
       let payload = message.toString();
       // debug raw message
       console.log(`Received message ${payload} from topic ${topic}`);
-      if (regexpTopic.test(topic) && regexpPayload.test(payload)) {
-        let rawData = regexpPayload.exec(payload);
-        let data: DhtDataInterface = {
-          deviceName: regexpTopic.exec(topic)![1],
-          timestamp: Number(rawData![3]),
-          temperature: Number(rawData![1]),
-          humidity: Number(rawData![2]),
-        };
-        // update data if topic matches selected sensor
-        if (this.selectedSensor?.topic === data.deviceName) {
-          this.temperature = data.temperature;
-          this.humidity = data.humidity;
-          this.timestamp = data.timestamp;
-          this.isData = true;
-        }
-        // record latest data
-        let deviceIndex = this.sensors.findIndex(
-          (sensor) => sensor.topic === data.deviceName
-        );
-        console.log(deviceIndex);
-        if (deviceIndex !== -1) {
-          this.sensors[deviceIndex].data = data;
+      // extract device name and data
+      let regexpTopic = new RegExp('purdue-dac/sensor/(.*)');
+      let deviceName = regexpTopic.exec(topic);
+      let sensor = this.sensors.find(
+        (sensor) => sensor.topic === deviceName![1]
+      );
+      if (sensor?.type === 'dht') {
+        let regexpPayload = new RegExp('(.*):(.*):(.*)');
+        if (regexpTopic.test(topic) && regexpPayload.test(payload)) {
+          let rawData = regexpPayload.exec(payload);
+          let data: DhtDataInterface = {
+            deviceName: regexpTopic.exec(topic)![1],
+            timestamp: Number(rawData![3]),
+            temperature: Number(rawData![1]),
+            humidity: Number(rawData![2]),
+          };
+          // update data if topic matches selected sensor
+          if (this.selectedSensor?.topic === data.deviceName) {
+            this.isData = true;
+            this.timestamp = data.timestamp;
+            this.humidity = data.humidity;
+            this.temperature = data.temperature;
+          }
+
+          // record latest data
+          let deviceIndex = this.sensors.findIndex(
+            (sensor) => sensor.topic === data.deviceName
+          );
+          if (deviceIndex !== -1) {
+            this.sensors[deviceIndex].data = data;
+          }
         }
       }
     });
@@ -103,12 +115,20 @@ export class AgWeekComponent {
     let selectedSensorIndex = this.sensors.findIndex(
       (sensor) => sensor.topic === event.value.topic
     );
-    if (selectedSensorIndex !== -1) {
-      this.deviceName = this.sensors[selectedSensorIndex].name;
-      this.temperature = this.sensors[selectedSensorIndex].data?.temperature;
-      this.humidity = this.sensors[selectedSensorIndex].data?.humidity;
-      this.timestamp = this.sensors[selectedSensorIndex].data?.timestamp;
-      this.isData = true;
+    if (
+      selectedSensorIndex !== -1 &&
+      this.sensors[selectedSensorIndex].type === 'dht'
+    ) {
+      let data = this.sensors[selectedSensorIndex].data as DhtDataInterface;
+      if (data === undefined) {
+        this.isData = false;
+      } else {
+        this.deviceName = this.sensors[selectedSensorIndex].name;
+        this.temperature = data.temperature;
+        this.humidity = data.humidity;
+        this.timestamp = data.timestamp;
+        this.isData = true;
+      }
     }
     this.selectedSensor = this.sensors[selectedSensorIndex];
   }
